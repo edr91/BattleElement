fa66c3912dfd4bcbf8028a346405f277816a7bba
Emanuel Degro <emanuel.degro@gmail.com>
2023-10-31 16:03:09 -0400

    an update to the buttons

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-alert("Wencome to Battle Element !")
+function selecMonsterPlayer() {
+    alert('you chose this monster')
+}
+
+let buttonMonsterplayer = document.getElementById('choose-monster')
+buttonMonsterplayer.addEventListener('click', selecMonsterPlayer)
